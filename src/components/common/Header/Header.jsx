6f41f0070c85966e0b07686ff9730c837c0f756b
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-13 23:33:39 +0900

    Feat: Header 컴포넌트 UI 구현

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from '@/components/common/Header/Header.styled';
+
+const Header = () => {
+  return <Styled.Header>Organization Name / Repository Name</Styled.Header>;
+};
+
+export default Header;
---
+++ src/components/common/Header/Header.styled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Header = styled.header`
+  width: 100%;
+  height: 72px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  border-bottom: 3px solid #000;
+`;
+
+export { Header };
