284c30601cb7d5c4ff0064844af448daef4f29f4
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-14 09:13:06 +0900

    Feat: Loading UI 수정

--- src/components/common/Loading/Loading.jsx
+++ src/components/common/Loading/Loading.jsx
@@ -1,39 +1,13 @@
 import React from 'react';
 import Skeleton from '@mui/material/Skeleton';
-import Grid from '@mui/material/Grid';
 
 const Loading = () => {
   return (
-    <Grid width="100%" margin="0" container alignItems="center">
-      <Grid item xs={9}>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={50} animation="wave" />
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={30} animation="wave" />
-      </Grid>
-      <Grid item xs>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={40} animation="wave" />
-      </Grid>
-      <Grid item xs={9}>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={50} animation="wave" />
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={30} animation="wave" />
-      </Grid>
-      <Grid item xs>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={40} animation="wave" />
-      </Grid>
-      <Grid item xs={9}>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={50} animation="wave" />
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={30} animation="wave" />
-      </Grid>
-      <Grid item xs>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={40} animation="wave" />
-      </Grid>
-      <Grid item xs={9}>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={50} animation="wave" />
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={30} animation="wave" />
-      </Grid>
-      <Grid item xs>
-        <Skeleton sx={{ bgcolor: 'grey.300' }} width="95%" height={40} animation="wave" />
-      </Grid>
-    </Grid>
+    <>
+      <Skeleton sx={{ bgcolor: 'grey.300' }} height={50} animation="wave" />
+      <Skeleton sx={{ bgcolor: 'grey.300' }} height={30} animation="wave" />
+      <Skeleton sx={{ bgcolor: 'grey.300' }} height={25} animation="wave" />
+    </>
   );
 };
 
