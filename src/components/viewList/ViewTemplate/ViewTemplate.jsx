dc6ee405bce0cf3bbb315f5ab285a8cfb938127b
Taak-e <kjtag0408@gmail.com>
2022-09-15 00:50:21 +0900

    Chore: develop merge를 위해 커밋

--- src/components/viewList/ViewTemplate/ViewTemplate.jsx
+++ src/components/viewList/ViewTemplate/ViewTemplate.jsx
@@ -11,12 +11,11 @@ const ViewTemplate = () => {
   useEffect(() => {
     fetchIssues();
   }, [fetchIssues]);
-
   return (
     <Box sx={{ border: '4px dashed grey' }}>
       <List sx={{ width: '100%', minWidth: 545, bgcolor: 'background.paper' }}>
-        {issues.map((issueInfo, id) => {
-          return <ViewListItem issueInfo={issueInfo} key={id} />;
+        {issues.map((issueInfo, number) => {
+          return <ViewListItem issueInfo={issueInfo} key={number} />;
         })}
       </List>
     </Box>
@@ -24,3 +23,42 @@ const ViewTemplate = () => {
 };
 
 export default ViewTemplate;
+
+// import React, { useEffect, useRef } from 'react';
+// import { Box, List } from '@mui/material';
+// import useIssuesActions from '@/hooks/useIssuesActions';
+// import useIssuesStore from '@/hooks/useIssuesStore';
+// import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+// import ViewListItem from '@/components/viewList/ViewListItem/ViewListItem';
+
+// const ViewTemplate = () => {
+//   const { fetchIssues } = useIssuesActions();
+//   const issues = useIssuesStore();
+
+//   const targetRef = useRef(null);
+//   const fetchMoreItemsFunc = () => fetchIssues();
+
+//   const [isFetching, setIsFetching] = useInfiniteScroll(targetRef, fetchMoreItemsFunc);
+
+//   useEffect(() => {
+//     fetchIssues();
+
+//     if (!isFetching) return;
+
+//     fetchMoreItemsFunc();
+//   }, [fetchIssues]);
+//   return (
+//     <>
+//       <Box sx={{ border: '4px dashed grey' }}>
+//         <List sx={{ width: '100%', minWidth: 545, bgcolor: 'background.paper' }}>
+//           {issues.map((issueInfo, number) => {
+//             return <ViewListItem issueInfo={issueInfo} key={number} />;
+//           })}
+//         </List>
+//       </Box>
+//       <div ref={targetRef} />
+//     </>
+//   );
+// };
+
+// export default ViewTemplate;
