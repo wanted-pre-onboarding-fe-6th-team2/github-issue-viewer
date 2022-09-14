13d3364faf427d23ea70493314407debbddbff8f
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-14 19:10:28 +0900

    Fix: Layout에 Header 적용

--- src/components/common/Layout/Layout.jsx
+++ src/components/common/Layout/Layout.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Container from '@mui/material/Container';
+import Header from '@/components/common/Header/Header';
 
 const Layout = ({ children }) => {
-  return <Container maxWidth="sm">{children}</Container>;
+  return (
+    <Container maxWidth="sm">
+      <Header />
+      {children}
+    </Container>
+  );
 };
 
 export default Layout;
