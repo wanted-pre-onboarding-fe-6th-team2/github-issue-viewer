b08f2923efe12029a1d63b8f2fc4d345b2f69969
HaJunRyu <hjr4557@naver.com>
2022-09-14 19:16:50 +0900

    Feat: 이슈 상세 목록을 관리 할 context를 정의하고 앱 전역에 적용한다

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import IssueList from '@/pages/IssueList/IssueList';
 import IssueDetail from '@/pages/IssueDetail/IssueDetail';
 import { ROUTES } from '@/constants/route';
 import IssuesProvider from '@/context/IssuesProvider';
+import IssueDetailsProvider from './context/IssueDetailsProvider';
 
 const App = () => (
   <>
     <Global styles={resetCss} />
 
     <IssuesProvider>
-      <Routes>
-        <Route path={ROUTES.HOME} element={<Home />} />
-        <Route path={ROUTES.ISSUE_LIST} element={<IssueList />} />
-        <Route path={`${ROUTES.ISSUE_DETAIL}/:issueId`} element={<IssueDetail />} />
-      </Routes>
+      <IssueDetailsProvider>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.ISSUE_LIST} element={<IssueList />} />
+          <Route path={`${ROUTES.ISSUE_DETAIL}/:issueId`} element={<IssueDetail />} />
+        </Routes>
+      </IssueDetailsProvider>
     </IssuesProvider>
   </>
 );
