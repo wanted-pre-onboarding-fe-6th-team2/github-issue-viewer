8ce45e39d268283267d6e012b1c0d979a99ada84
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-14 14:58:26 +0900

    Fix: Header 라우트 상수화 처리

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as Styled from '@/components/common/Header/Header.styled';
+import { ROUTES } from '@/constants/route';
 
 const Header = () => {
   return (
     <Styled.Header>
-      <Styled.GnbLink to="/">Angular / Angular-cli</Styled.GnbLink>
+      <Styled.GnbLink to={ROUTES.HOME}>Angular / Angular-cli</Styled.GnbLink>
     </Styled.Header>
   );
 };
